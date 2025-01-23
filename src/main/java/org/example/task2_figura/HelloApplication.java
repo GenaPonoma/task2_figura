64cf8ddf31b0a56f0203705c6ccdfa48fbda5e74
gennadii_ponomarev <g.ponomareow2014@yandex.ru>
2025-01-23 21:22:34 +0300

    delete class

--- src/main/java/org/example/task2_figura/HelloApplication.java
+++ src/main/java/org/example/task2_figura/HelloApplication.java
@@ -11,13 +11,34 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 700, 600);
+        stage.setTitle("Рисование фигур!");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
         launch();
+
+
+    }
+}
+
+class Bmi {
+    private float height, weight;
+
+    public Bmi(float height, float weight) {
+        this.height = height;
+        this.weight = weight;
+    }
+    public float getBMI() {
+        return weight / this.height / this.height * 10000;
+    }
+    public float getHeight() {
+        return height;
+    }
+    @Override
+    public String toString () {
+        return "Bmi =" + String.format("%. 1f", getBMI()) + " ";
     }
 }
